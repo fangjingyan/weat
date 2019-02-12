2e8811f2efc073ff9124a1a96a2cfd9def4c6f37
Hanxiao Hu <martinhu1994@gmail.com>
2019-02-12 18:36:36 -0500

    modification

--- src/main/java/ca/uwaterloo/ece651/repository/RestaurantRepository.java
+++ src/main/java/ca/uwaterloo/ece651/repository/RestaurantRepository.java
@@ -2,9 +2,11 @@ package ca.uwaterloo.ece651.repository;
 
 import ca.uwaterloo.ece651.model.Restaurant;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
     List<Restaurant> findByName(String name);
 }
