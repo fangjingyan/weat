fa9ad80561e38439388b846a25610356c72a7f0b
fangjingyan <43054004+fangjingyan@users.noreply.github.com>
2019-02-14 13:06:33 -0500

    Feature wang selection (#4)
    Add client project into the repo.

---
+++ Client/app/src/main/java/com/example/olivia/weat/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.olivia.weat;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_selectrestaurant:
+                    mTextMessage.setText("select");
+                    return true;
+                case R.id.navigation_cookyourself:
+                    mTextMessage.setText("cook");
+                    return true;
+                case R.id.navigation_blog:
+                    mTextMessage.setText("blog");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
