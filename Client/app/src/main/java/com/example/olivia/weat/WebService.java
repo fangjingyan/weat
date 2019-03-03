ea2d28fd51dac951a9d5c2ba3e1fff272c4e547b
gitofwwr <wwr0706@gmail.com>
2019-03-03 13:04:41 +0800

    have connected to sever, but need futher tested

--- Client/app/src/main/java/com/example/olivia/weat/WebService.java
+++ Client/app/src/main/java/com/example/olivia/weat/WebService.java
@@ -5,14 +5,19 @@ package com.example.olivia.weat;
 import android.util.Log;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WebService {
-    private static String servlet = "https://powerful-bastion-16516.herokuapp.com/";
+    private static String servlet = "https://powerful-bastion-16516.herokuapp.com/restaurant?Category=Korean";
 
     static private List<String> restaurants = new ArrayList<String>(){{
         add("Restaurant1"); add("Restaurant2"); add("Restaurant3");
@@ -20,11 +25,13 @@ public class WebService {
     }};
 
     public static List<String> executeHttpGet(List<String> Info){
-        String path = servlet + "?";
-        for(String str : Info){
-            path = path + str + "&";
-        }
-        path = path.substring(0, path.length() - 1);
+//        String path = servlet + "?";
+//        for(String str : Info){
+//            path = path + str + "&";
+//        }
+//        path = path.substring(0, path.length() - 1);
+        String path = servlet;
+        Log.e("path is :", path);
         try {
             Log.d("url", path);
             URL url = new URL(path);
@@ -34,14 +41,23 @@ public class WebService {
             conn.setDoInput(true);
             conn.setRequestMethod("GET");
             if (conn.getResponseCode() == 200) {
+                Log.e("get", "get response");
                 InputStream is = conn.getInputStream();
-                String res = IOUtils.toString(is, "utf-8");
-                Log.d("res in WebService", res);
+                JSONParser jsonParser = new JSONParser();
+                org.json.simple.JSONArray array = (org.json.simple.JSONArray)jsonParser.parse(new InputStreamReader(is, "UTF-8"));
+                for(Object obj : array){
+                    org.json.simple.JSONObject jsObj = (org.json.simple.JSONObject) obj;
+                    Log.e("get name from sever", (String)jsObj.get("name"));
+                }
+//                String res = IOUtils.toString(is, "utf-8");
+                Log.e("res in WebService", "jsonObject is :"+array);
+//                Log.e("res in WebService", res);
                 return restaurants;
             }
         }catch (Exception e){
             e.printStackTrace();
         }
-        return null;
+        Log.e("WebService Error", "do not get message from sever");
+        return restaurants;
     }
 }
