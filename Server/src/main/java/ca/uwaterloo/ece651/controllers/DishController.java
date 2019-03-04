8643395036dfde27c11d0694865fd5c0de2e95ed
Di Li <ldi@uwaterloo.ca>
2019-03-04 13:45:55 -0500

    fix some issues

--- Server/src/main/java/ca/uwaterloo/ece651/controllers/DishController.java
+++ Server/src/main/java/ca/uwaterloo/ece651/controllers/DishController.java
@@ -32,12 +32,59 @@ public class DishController {
              @RequestParam(value = "Green_pepper", defaultValue = "_") String green_pepper,
              @RequestParam(value = "Corn", defaultValue = "_") String corn,
              @RequestParam(value = "Broccoli", defaultValue = "_") String broccoli,
-             @RequestParam(value = "Dou_fu", defaultValue = "_") String dou_fu
+             @RequestParam(value = "Dou_fu", defaultValue = "_") String douFu
 			 )
 	 {
 		 List<Dish> result = null;
-		 String ingredients=null;
-		 ingredients=pork+beef+fish+mutton+chicken+egg+potato+tomato+cabbage+onion+spinach+green_pepper+corn+broccoli+dou_fu;
+		 String ingredients = null;
+		 
+		 if(pork.equals("Pork")){
+			 pork = "1";
+		 }
+         if(beef.equals("Beef")){
+        	 beef = "1";
+		 }
+         if(fish.equals("Fish")){
+        	 fish = "1";
+		 }
+         if(pork.equals("Mutton")){
+        	 pork = "1";
+		 }
+         if(mutton.equals("Chicken")){
+        	 mutton = "1";
+		 }
+         if(egg.equals("Egg")){
+        	 egg = "1";
+      	 }
+         if(potato.equals("Potato")){
+        	 potato = "1";
+      	 }
+         if(tomato.equals("Tomato")){
+        	 tomato = "1";
+      	 }
+         if(cabbage.equals("Cabbage")){
+        	 cabbage = "1";
+      	 }
+         if(onion.equals("Onion")){
+        	 onion = "1";
+      	 }
+         if(spinach.equals("Spinach")){
+        	 spinach = "1";
+      	 }
+         if(green_pepper.equals("Green_pepper")){
+        	 green_pepper = "1";
+      	 }
+         if(corn.equals("Corn")){
+        	 corn = "1";
+      	 }
+         if(broccoli.equals("Broccoli")){
+        	 broccoli = "1";
+		 }
+         if(douFu.equals("Dou_fu")){
+        	 douFu = "1";
+		 }
+         
+		 ingredients = pork + beef + fish +mutton + chicken + egg + potato + tomato + cabbage + onion + spinach + green_pepper + corn+broccoli + douFu;
 		 result = dishRepository.findByIngredientsLike(ingredients);
 		 return result;
 		 
