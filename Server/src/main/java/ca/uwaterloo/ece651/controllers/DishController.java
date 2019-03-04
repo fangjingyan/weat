6e47c7ae745d0534d9d2b82bafe8727b8ae0de93
li15t <ldi@uwaterloo.ca>
2019-03-04 14:02:24 -0500

    add more features for dish (#23)
    * add more features for dish
    
    * fix some issues

--- Server/src/main/java/ca/uwaterloo/ece651/controllers/DishController.java
+++ Server/src/main/java/ca/uwaterloo/ece651/controllers/DishController.java
@@ -1,7 +1,10 @@
 package ca.uwaterloo.ece651.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.uwaterloo.ece651.models.Dish;
@@ -15,10 +18,80 @@ public class DishController {
 	 private DishRepository dishRepository;
 	 
 	 @RequestMapping("/dish")
-	 public String Dish() {
-	 	return "this is dish";
+	 public List<Dish> getDishs(@RequestParam(value = "Pork", defaultValue = "_") String pork,
+             @RequestParam(value = "Beef", defaultValue = "_") String beef,
+             @RequestParam(value = "Fish", defaultValue = "_") String fish,
+             @RequestParam(value = "Mutton", defaultValue = "_") String mutton,
+             @RequestParam(value = "Chicken", defaultValue = "_") String chicken,
+             @RequestParam(value = "Egg", defaultValue = "_") String egg,
+             @RequestParam(value = "Potato", defaultValue = "_") String potato,
+             @RequestParam(value = "Tomato", defaultValue = "_") String tomato,
+             @RequestParam(value = "Cabbage", defaultValue = "_") String cabbage,
+             @RequestParam(value = "Onion", defaultValue = "_") String onion,
+             @RequestParam(value = "Spinach", defaultValue = "_") String spinach,
+             @RequestParam(value = "Green_pepper", defaultValue = "_") String green_pepper,
+             @RequestParam(value = "Corn", defaultValue = "_") String corn,
+             @RequestParam(value = "Broccoli", defaultValue = "_") String broccoli,
+             @RequestParam(value = "Dou_fu", defaultValue = "_") String douFu
+			 )
+	 {
+		 List<Dish> result = null;
+		 String ingredients = null;
+		 
+		 if(pork.equals("Pork")){
+			 pork = "1";
+		 }
+         if(beef.equals("Beef")){
+        	 beef = "1";
+		 }
+         if(fish.equals("Fish")){
+        	 fish = "1";
+		 }
+         if(pork.equals("Mutton")){
+        	 pork = "1";
+		 }
+         if(mutton.equals("Chicken")){
+        	 mutton = "1";
+		 }
+         if(egg.equals("Egg")){
+        	 egg = "1";
+      	 }
+         if(potato.equals("Potato")){
+        	 potato = "1";
+      	 }
+         if(tomato.equals("Tomato")){
+        	 tomato = "1";
+      	 }
+         if(cabbage.equals("Cabbage")){
+        	 cabbage = "1";
+      	 }
+         if(onion.equals("Onion")){
+        	 onion = "1";
+      	 }
+         if(spinach.equals("Spinach")){
+        	 spinach = "1";
+      	 }
+         if(green_pepper.equals("Green_pepper")){
+        	 green_pepper = "1";
+      	 }
+         if(corn.equals("Corn")){
+        	 corn = "1";
+      	 }
+         if(broccoli.equals("Broccoli")){
+        	 broccoli = "1";
+		 }
+         if(douFu.equals("Dou_fu")){
+        	 douFu = "1";
+		 }
+         
+		 ingredients = pork + beef + fish +mutton + chicken + egg + potato + tomato + cabbage + onion + spinach + green_pepper + corn+broccoli + douFu;
+		 result = dishRepository.findByIngredientsLike(ingredients);
+		 return result;
+		 
 	 }
-
+	 
+	 
+	 
 	 @RequestMapping("/get")
 	 public String test4() {
         Iterable<Dish> dishes = dishRepository.findAll();
@@ -29,3 +102,5 @@ public class DishController {
 	 }
 	
 }
+
+	 
