9deddbf959f4220198366e8c29096f5d14652a87
Pari <Parinaz.hsn@gmail.com>
2019-03-16 20:08:12 -0400

    Add ProfileRepository and Model

--- Server/src/main/java/ca/uwaterloo/ece651/models/Profile.java
+++ Server/src/main/java/ca/uwaterloo/ece651/models/Profile.java
@@ -1,5 +1,7 @@
 package ca.uwaterloo.ece651.models;
 
+//import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,42 +9,55 @@ import javax.persistence.Id;
 
 @Entity
 public class Profile {
+	
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
     private String email;
     private String password;
-    private String nickname;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
+    private String username;
+    
+//    private Set<Role> roles;
+    
+/*    private String active;
+    
+	public String getActive() {
+		return active;
+	}
+	public void setActive(String active) {
+		this.active = active;
+	}
+*/
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	@Override
+	public String toString() {
+		return "Profile [email=" + email + ", password=" + password + ", username=" + username + "]";
+	}
+   
+	
+	
 }
