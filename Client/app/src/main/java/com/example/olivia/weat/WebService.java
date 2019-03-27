e2c5fd3381305d21cedd901826ca23c142ed8f8c
Jingyan Fang <jingyan727@gmail.com>
2019-03-26 21:25:46 -0400

    Update WebService.java

--- Client/app/src/main/java/com/example/olivia/weat/WebService.java
+++ Client/app/src/main/java/com/example/olivia/weat/WebService.java
@@ -166,7 +166,7 @@ public class WebService {
                     User.addMeal(time, content);
                 }
             }
-            
+
 
             return true;
         }catch(Exception e){
